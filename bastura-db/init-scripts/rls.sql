d64c8773df47ceb622bd77508233b06d9db56789
EnBee <nplrahman922@com>
2026-08-08 23:53:29 +0800

    chore: setup monorepo structure

---
+++ bastura-db/init-scripts/rls.sql
@@ -0,0 +1,236 @@
+ALTER TABLE users ENABLE ROW LEVEL SECURITY;
+ALTER TABLE waste_catalog ENABLE ROW LEVEL SECURITY;
+ALTER TABLE roles ENABLE ROW LEVEL SECURITY;
+ALTER TABLE waste_category ENABLE ROW LEVEL SECURITY;
+ALTER TABLE announcements ENABLE ROW LEVEL SECURITY;
+ALTER TABLE education_content ENABLE ROW LEVEL SECURITY;
+ALTER TABLE simba_content ENABLE ROW LEVEL SECURITY;
+ALTER TABLE balance ENABLE ROW LEVEL SECURITY;
+ALTER TABLE deposit ENABLE ROW LEVEL SECURITY;
+ALTER TABLE withdrawals ENABLE ROW LEVEL SECURITY;
+ALTER TABLE split_bills ENABLE ROW LEVEL SECURITY;
+ALTER TABLE sb_allocations ENABLE ROW LEVEL SECURITY;
+ALTER TABLE transactions ENABLE ROW LEVEL SECURITY;
+ALTER TABLE transaction_logs ENABLE ROW LEVEL SECURITY;
+ALTER TABLE audit_logs ENABLE ROW LEVEL SECURITY;
+ALTER TABLE error_logs ENABLE ROW LEVEL SECURITY;
+ALTER TABLE update_logs ENABLE ROW LEVEL SECURITY;
+
+
+-- policy punya manajemen akun ya (bisa di modif lagi nanti) T_T
+
+CREATE POLICY "user_lihat_profil_sendiri" ON users
+FOR SELECT
+USING (
+    id_users = current_setting('app.current_user_id', true)::uuid
+    AND deleted_at IS NULL 
+    AND status_active != 'deleted' 
+    AND status_active != 'blocked'
+);
+
+CREATE POLICY "user_update_profil_sendiri" ON users
+FOR UPDATE
+USING (id_users = current_setting('app.current_user_id', true)::uuid);
+
+CREATE POLICY "admin_lihat_semua_user" ON users
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'admin');
+
+CREATE POLICY "super_admin_bisa_update_semua_kecuali_super_admin_lain" ON users
+FOR UPDATE
+USING (
+    current_setting('app.current_user_role', true) = 'super admin'
+    AND (
+        id_users = current_setting('app.current_user_id', true)::uuid
+        OR
+        role_id != (SELECT id_roles FROM roles WHERE roles = 'super admin' LIMIT 1)
+    )
+);
+
+CREATE POLICY "admin_bisa_update_user_saja" ON users
+FOR UPDATE
+USING (
+    current_setting('app.current_user_role', true) = 'admin'
+    AND (
+        id_users = current_setting('app.current_user_id', true)::uuid
+        OR
+        role_id = (SELECT id_roles FROM roles WHERE roles = 'users' LIMIT 1)
+    )
+);
+
+-- policy manajemen catalog
+
+CREATE POLICY "admin_bisa_tambah_catalog" ON waste_catalog
+FOR INSERT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_update_catalog" ON waste_catalog
+FOR UPDATE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_delete_catalog" ON waste_catalog
+FOR DELETE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_tambah_category" ON waste_category
+FOR INSERT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_update_category" ON waste_category
+FOR UPDATE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting   ('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_delete_category" ON waste_category
+FOR DELETE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "user_bisa_lihat_all_catalog" ON waste_catalog
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'user' OR current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_lihat_all_category" ON waste_category
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'user' OR current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+-- policy manajemen pengumuman
+
+CREATE POLICY "admin_bisa_tambah_pengumuman" ON announcements
+FOR INSERT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_update_pengumuman" ON announcements
+FOR UPDATE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_delete_pengumuman" ON announcements
+FOR DELETE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "user_bisa_lihat_pengumuman" ON announcements
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'user' OR current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+-- policy manajemen edukasi
+
+CREATE POLICY "admin_bisa_tambah_edukasi" ON education_content
+FOR INSERT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_update_edukasi" ON education_content
+FOR UPDATE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_delete_edukasi" ON education_content
+FOR DELETE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "user_bisa_lihat_edukasi" ON education_content
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'user' OR current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+-- policy manajemen simba content
+
+CREATE POLICY "admin_bisa_tambah_simba" ON simba_content
+FOR INSERT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_update_simba" ON simba_content
+FOR UPDATE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_delete_simba" ON simba_content
+FOR DELETE
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+CREATE POLICY "admin_bisa_lihat_simba" ON simba_content
+FOR SELECT
+USING (current_setting('app.current_role', true) = 'admin' OR current_setting('app.current_role', true) = 'super admin');
+
+-- policy manajemen transaksi
+
+CREATE POLICY "hanya_user_bisa_melihat_riwayat_deposit_dirinya_sendiri" ON deposit
+FOR SELECT
+USING (id_user = current_setting('app.current_user_id', true)::uuid);
+
+CREATE POLICY "admin_bebas_melihat_semua_deposit" ON deposit
+FOR SELECT
+USING (
+    current_setting('app.current_user_role', true) IN ('admin', 'super admin')
+);
+
+CREATE POLICY "admin_bisa_insert_deposit" ON deposit
+FOR INSERT
+USING (
+    current_setting('app.current_user_role', true) IN ('admin', 'super admin')
+);
+
+CREATE POLICY "admin_bisa_update_deposit" ON deposit
+FOR UPDATE
+USING (
+    current_setting('app.current_user_role', true) IN ('admin', 'super admin')
+);
+
+CREATE POLICY "user_bisa_melihat_riwayat_withdrawal_dirinya_sendiri" ON withdrawals
+FOR SELECT
+USING (id_user = current_setting('app.current_user_id', true)::uuid);
+
+CREATE POLICY "admin_bisa_melihat_semua_withdrawal" ON withdrawals
+FOR SELECT
+USING (
+    current_setting('app.current_user_role', true) IN ('admin', 'super admin')
+);
+
+CREATE POLICY "user_bisa_insert_withdrawal" ON withdrawals
+FOR INSERT
+USING (
+    current_setting('app.current_user_role', true) = 'user'
+);
+
+CREATE POLICY "admin_bisa_update_withdrawal" ON withdrawals
+FOR UPDATE
+USING (
+    current_setting('app.current_user_role', true) IN ('admin', 'super admin')
+);
+
+CREATE POLICY "user_bisa_update_status_withdrawal_diri_sendiri" ON withdrawals
+FOR UPDATE
+USING (
+    id_user = current_setting('app.current_user_id', true)::uuid
+    AND wd_status = 'processed'
+);
+
+CREATE POLICY "admin_bisa_apapun_splitbill" ON split_bills
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('admin', 'super admin'));
+
+CREATE POLICY "admin_bisa_apapun_sb_allocation" ON sb_allocations
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('admin', 'super admin'));
+
+-- policy untuk logging
+
+CREATE POLICY "super_admin_bisa_apaaja_audit_logs" ON audit_logs
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin'));
+
+CREATE POLICY "super_admin_bisa_apaaja_error_logs" ON error_logs
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin'));
+
+CREATE POLICY "super_admin_bisa_apaaja_transaction_logs" ON transaction_logs
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin'));
+
+CREATE POLICY "super_admin_bisa_apaaja_transaction" ON transactions
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin')); 
+
+-- policy profit dan fee
+
+CREATE POLICY "super_admin_bisa_apaaja_profit" ON profit
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin'));
+
+CREATE POLICY "super_admin_bisa_apaaja_fee" ON fee
+FOR ALL
+USING (current_setting('app.current_role', true) IN ('super admin'));
---
+++ bastura-db/init-scripts/schema.sql
@@ -0,0 +1,205 @@
+-- create database bastura;
+
+
+CREATE TYPE status_users AS ENUM('active', 'deleted', 'blocked');
+CREATE TYPE role_s AS ENUM('active','deactive');
+CREATE TYPE trans_type AS ENUM('deposit','withdrawal' ,'splitbills');
+CREATE TYPE status_tf AS ENUM('processed', 'canceled', 'success' , 'rejected' , 'deleted');
+
+-- buat type enum duluan T_T
+
+CREATE TABLE IF NOT EXISTS roles(
+    id_roles SERIAL PRIMARY KEY NOT NULL,
+    roles VARCHAR(50) NOT NULL,
+    status_role role_s NOT NULL DEFAULT 'active'
+);
+
+CREATE TABLE IF NOT EXISTS waste_category(
+    id_waste_category SERIAL PRIMARY KEY NOT NULL,
+    category_name VARCHAR(50) NOT NULL,
+    ct_description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS announcements(
+    id_announcements UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    content jsonb,
+    announcements_img TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS education_content(
+    id_content UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    content jsonb,
+    education_img TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS simba_content(
+    id_simba UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    field TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS update_logs (
+    profile_up TIMESTAMPTZ NOT NULL,
+    transaction_up TIMESTAMPTZ NOT NULL,
+    announcements_up TIMESTAMPTZ NOT NULL,
+    education_up TIMESTAMPTZ NOT NULL,
+    simba_up TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS profit (
+    id_profit UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    amount_profit BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS fee (
+    id_fee UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    amount_fee BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ
+);
+
+-- table mandiri yang tidak bergantung satu sama lain beb t_t (DIATAS)
+
+CREATE TABLE IF NOT EXISTS users(
+    id_users UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    name VARCHAR(100),
+    phone VARCHAR(20), 
+    avatar_url TEXT,
+    role_id INT NOT NULL, 
+    status_active status_users DEFAULT 'active',
+    total_balance BIGINT DEFAULT 0 NOT NULL,
+    balance_held BIGINT,
+    blocked_at TIMESTAMPTZ, 
+    deleted_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+    reason_banned VARCHAR(200),
+
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(id_roles)
+);
+
+CREATE TABLE IF NOT EXISTS waste_catalog(
+    id_waste UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    category_id INT NOT NULL, 
+    unit VARCHAR(25) NOT NULL,
+    price BIGINT NOT NULL,
+    description TEXT,
+    catalog_img TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES waste_category(id_waste_category)
+);
+
+CREATE TABLE IF NOT EXISTS audit_logs(
+    id_audit SERIAL PRIMARY KEY NOT NULL,
+    actor_id UUID NOT NULL,
+    target_id UUID NOT NULL,
+    action_type VARCHAR(20) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,
+
+    CONSTRAINT fk_actor_id FOREIGN KEY (actor_id) REFERENCES users(id_users),
+    CONSTRAINT fk_target_id FOREIGN KEY (target_id) REFERENCES users(id_users)   
+);
+
+CREATE TABLE IF NOT EXISTS error_logs(
+    id_error SERIAL PRIMARY KEY NOT NULL,
+    error_message TEXT NOT NULL,
+    error_level VARCHAR(25) NOT NULL,
+    source VARCHAR(200),
+    error_at TIMESTAMPTZ NOT NULL DEFAULT now() 
+);
+
+CREATE TABLE IF NOT EXISTS transactions(
+    id_transaction UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    process_type trans_type NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS transaction_logs(
+    id_transaction SERIAL PRIMARY KEY NOT NULL,
+    id_tsc UUID NOT NULL,
+    tsc_type VARCHAR(25) NOT NULL,
+    processed_by UUID NOT NULL,
+    processed_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    detail_tsc TEXT,
+
+    CONSTRAINT fk_id_tsc FOREIGN KEY (id_tsc) REFERENCES transactions(id_transaction),
+    CONSTRAINT fk_processed_by FOREIGN KEY (processed_by) REFERENCES users(id_users)
+);
+
+CREATE TABLE IF NOT EXISTS balance(
+    id_balance UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    id_user UUID NOT NULL,
+    total_balance BIGINT NOT NULL,
+    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(), 
+
+    CONSTRAINT fk_id_user FOREIGN KEY (id_user) REFERENCES users(id_users),
+    CONSTRAINT cek_total_balance CHECK (total_balance >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS deposit (
+    id_deposit UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    id_user UUID NOT NULL,
+    catalog_id UUID NOT NULL,
+    weight_dp BIGINT NOT NULL,
+    dp_status status_tf DEFAULT 'processed',
+    dp_notes TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,  
+
+    CONSTRAINT fk_id_user_deposit FOREIGN KEY (id_user) REFERENCES users(id_users),
+    CONSTRAINT fk_catalog_id FOREIGN KEY (catalog_id) REFERENCES waste_catalog(id_waste)
+);
+
+CREATE TABLE IF NOT EXISTS withdrawals (
+    id_wd UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    id_user UUID NOT NULL,
+    amount BIGINT NOT NULL,
+    wd_status status_tf DEFAULT 'processed',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ,  
+
+    CONSTRAINT fk_id_user_wd FOREIGN KEY (id_user) REFERENCES users(id_users),
+    CONSTRAINT cek_amount CHECK (amount > 0)    
+);
+
+CREATE TABLE IF NOT EXISTS split_bills (
+    id_sb UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    total_sb BIGINT NOT NULL,
+    date_start TIMESTAMPTZ, 
+    date_end TIMESTAMPTZ,
+    remaining_sb BIGINT NOT NULL,
+    processed_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    processed_by UUID NOT NULL,
+
+    CONSTRAINT fk_processed_by_sb FOREIGN KEY (processed_by) REFERENCES users(id_users)
+);
+
+CREATE TABLE IF NOT EXISTS sb_allocations (
+    id_sb_allocations UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+    id_sb UUID NOT NULL,
+    id_user UUID NOT NULL,
+    final_amount BIGINT NOT NULL,
+    allocated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    CONSTRAINT fk_id_sb FOREIGN KEY (id_sb) REFERENCES split_bills(id_sb),
+    CONSTRAINT fk_id_user_sba FOREIGN KEY (id_user) REFERENCES users(id_users),
+    CONSTRAINT cek_final_amount CHECK (final_amount > 0)
+);
+
